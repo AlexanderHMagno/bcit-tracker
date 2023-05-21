48b44c6fe251327fbe6ec7836975bfbebbf28507
Alexander Hortua <alexander.hortua10@gmail.com>
2023-05-21 14:25:53 -0700

    Fixing error with dates

--- src/components/DueDate/index.tsx
+++ src/components/DueDate/index.tsx
@@ -12,11 +12,16 @@ interface props {
 export function DueDate({dueDate} : props) {
 
   const dueDateFormat = intervalToDuration({end: dueDate, start: new Date()});
-  const {days, hours} = dueDateFormat;
+  const {days = 0, hours = 0, years = 0 , months = 0} = dueDateFormat;
   
 
-  if (days) {
-    return <><span className={[styles.regularPill, styles.pill].join(" ")}>{"Due: " + (days + 1 )  + " days"}</span></>
+  
+
+  if (years || months || days) {
+    let totalDays = days? (days + 1) : 0;
+    totalDays += months? (months *  30) : 0;
+    totalDays += years ? years * 365 : 0;
+    return <><span className={[styles.regularPill, styles.pill].join(" ")}>{"Due: " + (totalDays)  + " days"}</span></>
   }
 
   if (hours) {
